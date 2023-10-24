df5705cb252f57e6676b760c1bc0c42fc9519c78
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-24 04:27:59 -0700

    basemodel update

--- models/base_model.py
+++ models/base_model.py
@@ -16,13 +16,13 @@ class BaseModel:
                     setattr(self, k, val)
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
-            if "created_at" != kwargs.get("created_at"):
-                self.created_at = datetime.now()
-            if "updated_at" != kwargs.get("updated_at"):
+            if "updated_at" not in kwargs:
                 self.updated_at = datetime.now()
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
         else:
-            self.if = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now())
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
