943ee48e9147bf46bfbe648d7ba65d6948d3ad65
Mimifresh <mimigolld@gmail.com>
2023-10-24 03:24:27 +0100

    task 9 review

--- models/review.py
+++ models/review.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = ""
-    user_id = ""
-    text = Column(
+    place_id = Column(String(60), ForeignKey("places_id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users_id"), nullable=False)
+    text = Column(String(1024), nullable=False)
