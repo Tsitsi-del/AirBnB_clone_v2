ad6cede38221047a7b8f117efe83e417a52a9a21
Mimifresh <mimigolld@gmail.com>
2023-10-24 03:16:02 +0100

    task 9 review

--- models/review.py
+++ models/review.py
@@ -3,8 +3,9 @@
 from models.base_model import BaseModel
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
+    __tablename__ = "reviews"
     place_id = ""
     user_id = ""
-    text = ""
+    text = Column(
