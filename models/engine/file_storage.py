636edc25953caecde767c5fb0a57229aa878f4c4
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-24 05:20:21 -0700

    delete update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class to manage file storage for hbnb clone"""
 import json
+import shlex
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.user import User
 
 
 class FileStorage:
@@ -8,7 +16,7 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         dictionary = {}
         if cls:
