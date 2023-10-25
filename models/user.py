f5b616009d4696cf660e03c1c27cb01dcdbcd863
Tsitsi-del <tsitsiduduzile@gmail.com>
2023-10-25 15:36:25 -0700

    update user

--- models/user.py
+++ models/user.py
@@ -15,5 +15,11 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-    places = relationship('Place', cascade="all, delete-orphan", backref="user")
-    reviews = relationship('Review', cascade="all, delete-orphan", backref='user')
+    places = relationship(
+                         'Place',
+                         cascade="all, delete-orphan",
+                         backref="user")
+    reviews = relationship(
+                           'Review',
+                           cascade="all, delete-orphan",
+                           backref='user')
